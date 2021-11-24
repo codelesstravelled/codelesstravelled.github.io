791796d0682368b8819aed65f9f2d3b60b835131
Martin Suarez <maasuarez@fi.uba.ar>
2021-11-24 01:27:55 -0300

    Nuevo directorio

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Min-cut problem variations",
+        "excerpt":"Last week I needed to solve a variation of the min-cut problem for flow networks. For those who don’t know about the minimum cut of a flow network I will give a brief explanation. Flow networks A flow network is a directed graph, tipically used to represent situations like traffic...","categories": ["min-cut","graph"],
+        "tags": [],
+        "url": "/min-cut/graph/2021/11/23/min-cut-problem-variations.html",
+        "teaser": "/assets/images/post1header.png"
+      }]
