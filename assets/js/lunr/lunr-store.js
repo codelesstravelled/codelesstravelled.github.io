1b8191c8a70a552865186c0c1b4503a0c9631273
Martin Suarez <maasuarez@fi.uba.ar>
2021-12-12 01:25:55 -0300

    New post, some errors corrected

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "Min-cut problem variations",
-        "excerpt":"Last week I needed to solve a variation of the min-cut problem for flow networks. For those who don’t know about the minimum cut of a flow network I will give a brief explanation. Flow networks A flow network is a directed graph, tipically used to represent situations like traffic...","categories": ["min-cut","graph"],
+        "excerpt":"Last week I needed to solve a variation of the min-cut problem for flow networks. For those who don’t know about the minimum cut of a flow network, I will give a brief explanation. Flow networks A flow network is a directed graph, typically used to represent situations like traffic...","categories": ["min-cut","graph"],
         "tags": [],
         "url": "/min-cut/graph/2021/11/23/min-cut-problem-variations.html",
         "teaser": "/assets/images/post1header.png"
+      },{
+        "title": "Solving Einstein riddle with Z3",
+        "excerpt":"Einstein five-houses consists, as the title says, of 5 houses. Each house has a certain color. Inside of each house, its owner has a pet, smokes a certain brand of cigars, drinks a certain beverage and has a nationality. Any of these is unique between the house owners. For a...","categories": ["python","z3","riddle"],
+        "tags": [],
+        "url": "/python/z3/riddle/2021/12/04/solving-Einstein-riddle-with-Z3.html",
+        "teaser": "/assets/images/post2header.png"
       }]
