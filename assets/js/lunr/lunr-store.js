2eddd5c8c1d688619a2f90cfcb46d3a7cd260024
Martin Suarez <maasuarez@fi.uba.ar>
2021-12-12 01:37:04 -0300

    Grammar mistakes

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": "/assets/images/post1header.png"
       },{
         "title": "Solving Einstein riddle with Z3",
-        "excerpt":"Einstein five-houses consists, as the title says, of 5 houses. Each house has a certain color. Inside of each house, its owner has a pet, smokes a certain brand of cigars, drinks a certain beverage and has a nationality. Any of these is unique between the house owners. For a...","categories": ["python","z3","riddle"],
+        "excerpt":"Einstein’s five-houses consists, as the title says, of 5 houses. Each house has a certain color. Inside of each house, its owner has a pet, smokes a certain brand of cigars, drinks a certain beverage, and has a nationality. Any of these is unique between the house owners. For a...","categories": ["python","z3","riddle"],
         "tags": [],
         "url": "/python/z3/riddle/2021/12/04/solving-Einstein-riddle-with-Z3.html",
         "teaser": "/assets/images/post2header.png"
