a345701b44d17a833c9a6a21d969e9963d7c511b
Martin Suarez <maasuarez@fi.uba.ar>
2022-01-17 03:18:56 -0300

    Camera fix

--- assets/js/header.js
+++ assets/js/header.js
@@ -106,9 +106,10 @@ var createScene = function () {
         });
     //}
 
+    
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 1.9, 3, new BABYLON.Vector3(0, 1, 0));
-    camera.attachControl(canvas, true);
-
+    //camera.attachControl(canvas, true);
+    
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, -1));
     
     const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:100, height:100});
